5f3c504f29cfa95d86a5b26c1d029902ccc0b605
north <blarvinius@gmail.com>
2018-08-25 20:17:22 +0100

    add index.js, sanitytest.js

---
+++ test/sanitytest.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+  , foo = 'bar'
+  , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+expect(foo).to.be.a('string');
+expect(foo).to.equal('bar');
+expect(foo).to.have.lengthOf(3);
+expect(beverages).to.have.property('tea').with.lengthOf(3);
