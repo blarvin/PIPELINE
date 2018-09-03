5b416d34b961315003b55f9dce5d9b60a7c7ec77
north <blarvinius@gmail.com>
2018-09-03 13:24:56 +0100

    Cypress setup test

---
+++ cypress/integration/examples/sample_spec.js
@@ -0,0 +1,7 @@
+describe('Initial setup proof test:', function () {
+    it('visits the app and finds some HTML on a page:', function () {
+        cy.visit('localhost:3000')
+
+        cy.contains('Lets go')
+    })
+})
