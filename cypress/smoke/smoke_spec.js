100ad02eefb6cf3748da013bc971a51b86cdb9fa
North Bremicker <blarvinius@gmail.com>
2018-11-02 10:31:22 +0000

    Revert "add post-deploy smoke test"
    This reverts commit b033acf23871391d3d829250f56a1da9e69aca47.

--- cypress/smoke/smoke_spec.js
+++
@@ -1,7 +0,0 @@
-describe('Smoke Test', function() {
-  it('Website is hosted and online', function() {
-    cy.visit('https://pipe-production.herokuapp.com/')
-
-    cy.get('h1').should('contain', 'pipe')
-  })
-})
