df36c50a63eae164e7866335f7eadb63f29a0d10
North Bremicker <blarvinius@gmail.com>
2018-11-10 13:50:26 +0000

    rename "/app" to "/src" has index.js.html.css

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+function CoolDogs (petDog) {
+    var dogs = {
+        small: ['chihuahua', 'whippit', 'shitzu', 'daschund'],
+        shaggy: ['burmese', 'collie', 'sheepdog'],
+        smart: ['bloodhound', 'GSP', 'komondor']
+    }
+
+    return !!dogs.smart.includes(petDog)
+}
+
+module.exports = CoolDogs
+console.log("PIPELINE entry point")
