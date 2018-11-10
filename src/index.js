b67188c6f2437b609e235abbc23a2d016b074a05
North Bremicker <blarvinius@gmail.com>
2018-11-10 14:54:05 +0000

    move dogs to /scripts

--- src/index.js
+++ src/index.js
@@ -6,17 +6,6 @@ const headline = "pipey McPipeface";
 ReactDOM.render(
     <div>{headline}</div>,
     document.getElementById('app')
-)
+);
 
-function CoolDogs (petDog) {
-    var dogs = {
-        small: ['chihuahua', 'whippit', 'shitzu', 'daschund'],
-        shaggy: ['burmese', 'collie', 'sheepdog'],
-        smart: ['bloodhound', 'GSP', 'komondor']
-    }
-
-    return !!dogs.smart.includes(petDog)
-}
-
-module.exports = CoolDogs
-console.log("PIPELINE entry point")
+console.log("PIPELINE entry point");
